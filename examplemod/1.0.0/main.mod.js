7771d0595b8c8e3991e15e247ac8dad2759b2cee
CR Jakob <78952659+CRJakob@users.noreply.github.com>
2025-05-27 17:16:19 +0000

    create app branch [no ci]

--- examplemod/1.0.0/main.mod.js
+++
@@ -1,7 +0,0 @@
-import { PolyMod } from "https://pml.orangy.cfd/PolyTrackMods/PolyModLoader/0.5.0/PolyModLoader.js";
-
-class ExampleMod extends PolyMod {
-    
-}
-
-export let polyMod = new ExampleMod();
